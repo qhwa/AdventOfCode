b217fbfc6ead9a995ea8a5e01c22a862f5f7da6f
qhwa <qhwa@163.com>
2019-11-23 09:26:09 +0800

    solve day2#2

--- lib/aoc/task/day02.ex
+++ lib/aoc/task/day02.ex
@@ -3,29 +3,31 @@ defmodule AOC.Task.Day02 do
   @see https://adventofcode.com/2015/day/2
   """
 
-  def resolve({:file, file}) do
+  def feet_of_wrapping_paper({:file, file}) do
     file
     |> File.stream!()
     |> Enum.to_list()
-    |> resolve()
+    |> feet_of_wrapping_paper()
   end
 
-  def resolve(list) when is_list(list) do
+  def feet_of_wrapping_paper(list) when is_list(list) do
     list
-    |> Enum.reduce(0, fn item, acc -> acc + resolve(item) end)
+    |> Enum.reduce(0, fn item, acc ->
+      acc + feet_of_wrapping_paper(item)
+    end)
   end
 
-  def resolve(data) when is_binary(data) do
+  def feet_of_wrapping_paper(data) when is_binary(data) do
     [l, w, h] =
       data
       |> String.trim()
       |> String.split("x")
       |> Enum.map(&String.to_integer/1)
 
-    resolve({l, w, h})
+    feet_of_wrapping_paper({l, w, h})
   end
 
-  def resolve({l, w, h}) when is_number(l) and is_number(w) and is_number(h) do
+  def feet_of_wrapping_paper({l, w, h}) when is_number(l) and is_number(w) and is_number(h) do
     2 * l * w + 2 * w * h + 2 * l * h + smallest_surface(l, w, h)
   end
 
@@ -34,6 +36,43 @@ defmodule AOC.Task.Day02 do
       l * min(w, h)
     else
       w * min(l, h)
+    end
+  end
+
+  def feet_of_ribbon({:file, file}) do
+    file
+    |> File.stream!()
+    |> Enum.to_list()
+    |> feet_of_ribbon()
+  end
+
+  def feet_of_ribbon(data) when is_binary(data) do
+    [l, w, h] = 
+      data
+      |> String.trim()
+      |> String.split("x")
+      |> Enum.map(&String.to_integer/1)
+    
+    feet_of_ribbon({l, w, h})
+  end
+
+  def feet_of_ribbon([]) do
+    0
+  end
+
+  def feet_of_ribbon([item | tail]) do
+    feet_of_ribbon(item) + feet_of_ribbon(tail)
+  end
+
+  def feet_of_ribbon({l, w, h}) do
+    smallest_sides(l, w, h) * 2 + l * w * h
+  end
+
+  defp smallest_sides(l, w, h) do
+    if l < w do
+      l + min(w, h)
+    else
+      w + min(l, h)
     end
   end
 end
--- test/tasks/day02_test.exs
+++ test/tasks/day02_test.exs
@@ -3,20 +3,33 @@ defmodule Day02Test do
 
   use ExUnit.Case
 
-  test "it works with single tuple" do
-    assert resolve({2, 3, 4}) == 58
-    assert resolve({1, 1, 10}) == 43
+  describe "feet_of_wrapping_paper/1" do
+    test "it works with single tuple" do
+      assert feet_of_wrapping_paper({2, 3, 4}) == 58
+      assert feet_of_wrapping_paper({1, 1, 10}) == 43
+    end
+
+    test "it works with lists" do
+      assert feet_of_wrapping_paper([{2, 3, 4}, {1, 1, 10}]) == 58 + 43
+    end
+
+    test "it works with single entry in binary" do
+      assert feet_of_wrapping_paper("2x3x4") == 58
+    end
+
+    test "it works with file stream" do
+      assert feet_of_wrapping_paper({:file, "test/fixtures/day02.txt"}) == 5259
+    end
   end
 
-  test "it works with lists" do
-    assert resolve([{2, 3, 4}, {1, 1, 10}]) == 58 + 43
-  end
+  describe "feet_of_ribbon/1" do
+    test "it works" do
+      assert feet_of_ribbon({2, 3, 4}) == 34
+      assert feet_of_ribbon([{2, 3, 4}, {1, 1, 10}]) == 48
+    end
 
-  test "it works single entry in binary" do
-    assert resolve("2x3x4") == 58
-  end
-
-  test "it works file stream" do
-    assert resolve({:file, "test/fixtures/day02.txt"}) == 5259
+    test "it works with file stream" do
+      assert feet_of_ribbon({:file, "test/fixtures/day02.txt"}) == 12_714
+    end
   end
 end
