e4dd26749aea7dca591800b15075e337ac21a9ca
qhwa <qhwa@163.com>
2019-11-23 09:58:11 +0800

    recover test

--- test/tasks/day03_test.exs
+++ test/tasks/day03_test.exs
@@ -5,8 +5,8 @@ defmodule Day03Test do
 
   describe "houses_visit/1" do
     test "it works" do
-      # assert houses_visit("") == 1
-      # assert houses_visit(">") == 2
+      assert houses_visit("") == 1
+      assert houses_visit(">") == 2
       assert houses_visit("^>v<") == 4
     end
   end
