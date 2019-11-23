b88574fa054258491ce733547d4fd0dd65d46877
qhwa <qhwa@163.com>
2019-11-23 13:55:19 +0800

    finish day3 part 2

--- lib/aoc/task/day03.ex
+++ lib/aoc/task/day03.ex
@@ -10,25 +10,78 @@ defmodule AOC.Task.Day03 do
   end
 
   def houses_visit(data) do
-    houses_visit(data, [0, 0], %{:"0_0" => 1}, 1)
+    houses_visit(data, [0, 0], %{:"0_0" => 1})
   end
 
-  def houses_visit("", _, _, count), do: count
+  def houses_visit("", _, map), do: Map.keys(map) |> length()
 
-  def houses_visit(<<dir::binary-size(1), rest::binary>>, pos, map, count) do
+  def houses_visit(<<dir::binary-size(1), rest::binary>>, pos, map) do
     pos = pos |> new_pos(dir)
-    key = pos |> Enum.join("_") |> String.to_atom()
+    houses_visit(rest, pos, record_visit(map, pos))
+  end
 
-    if Map.has_key?(map, key) do
-      houses_visit(rest, pos, map, count)
-    else
-      houses_visit(rest, pos, Map.put(map, key, 1), count + 1)
-    end
+  defp record_visit(map, pos) do
+    map
+    |> Map.put_new(key(pos), 1)
+  end
+
+  defp key(pos) do
+    pos
+    |> Enum.join("_")
+    |> String.to_atom()
   end
 
   defp new_pos([x, y], "^"), do: [x, y - 1]
   defp new_pos([x, y], "v"), do: [x, y + 1]
   defp new_pos([x, y], "<"), do: [x - 1, y]
   defp new_pos([x, y], ">"), do: [x + 1, y]
+
+  defp new_pos([x, y], ?^), do: [x, y - 1]
+  defp new_pos([x, y], ?v), do: [x, y + 1]
+  defp new_pos([x, y], ?<), do: [x - 1, y]
+  defp new_pos([x, y], ?>), do: [x + 1, y]
+
   defp new_pos(pos, _), do: pos
+
+  def houses_visit_with_robot(data) do
+    {santa_data, robot_data} = split_data(data)
+
+    santa_map = houses_visit_with_robot(santa_data, [0, 0], %{:"0_0" => 1})
+    robot_map = houses_visit_with_robot(robot_data, [0, 0], %{:"0_0" => 1})
+
+    santa_map
+    |> Map.merge(robot_map)
+    |> Map.keys()
+    |> length()
+  end
+
+  def houses_visit_with_robot([], _, map), do: map
+
+  # Santa moves first
+  def houses_visit_with_robot([dir | rest], pos, map) do
+    pos = pos |> new_pos(dir)
+    houses_visit_with_robot(rest, pos, record_visit(map, pos))
+  end
+
+  defp split_data(data) when is_binary(data) do
+    data
+    |> String.to_charlist()
+    |> split_data()
+  end
+
+  defp split_data(data) when is_list(data) do
+    split_data(data, [], [])
+  end
+
+  defp split_data([santa_dir, robot_dir | tail], santa, robot) do
+    split_data(tail, santa ++ [santa_dir], robot ++ [robot_dir])
+  end
+
+  defp split_data([santa_dir | tail], santa, robot) do
+    split_data(tail, santa ++ [santa_dir], robot)
+  end
+
+  defp split_data([], santa, robot) do
+    {santa, robot}
+  end
 end
--- test/tasks/day03_test.exs
+++ test/tasks/day03_test.exs
@@ -10,4 +10,15 @@ defmodule Day03Test do
       assert houses_visit("^>v<") == 4
     end
   end
+
+  describe "houses_visit_with_robot/1" do
+    test "it works" do
+      assert houses_visit_with_robot("") == 1
+      assert houses_visit_with_robot(">") == 2
+      assert houses_visit_with_robot("^^") == 2
+      assert houses_visit_with_robot("^v") == 3
+      assert houses_visit_with_robot("^>v<") == 3
+      assert houses_visit_with_robot("^v^v^v^v^v") == 11
+    end
+  end
 end
