bbc8dda5fc7c6401386e473d4204c3568b1c4b76
qhwa <qhwa@163.com>
2019-11-27 08:58:01 +0800

    finish day 12 part 2

--- lib/aoc/task/day12.ex
+++ lib/aoc/task/day12.ex
@@ -4,12 +4,42 @@ defmodule AOC.Task.Day12 do
   """
 
   def puzzle() do
-    str =
-      "priv/data/day12.txt"
-      |> File.read!()
+    "priv/data/day12.txt"
+    |> File.read!()
+    |> Jason.decode!()
+    |> walk()
+  end
 
-    ~r/[-\d]+/
-    |> Regex.scan(str)
-    |> Enum.reduce(0, fn [n], acc -> acc + String.to_integer(n) end)
+  def walk(%{} = obj) do
+    if valid?(obj) do
+      Enum.reduce(obj, 0, fn {_, v}, acc ->
+        walk(v) + acc
+      end)
+    else
+      0
+    end
+  end
+
+  def walk(prim) when is_integer(prim) do
+    prim
+  end
+
+  def walk(list) when is_list(list) do
+    Enum.reduce(list, 0, fn item, acc -> acc + walk(item) end)
+  end
+
+  def walk(str) when is_binary(str) do
+    0
+  end
+
+  def walk(_) do
+    0
+  end
+
+  def valid?(obj) do
+    obj
+    |> Map.values()
+    |> Enum.member?("red")
+    |> Kernel.!()
   end
 end
