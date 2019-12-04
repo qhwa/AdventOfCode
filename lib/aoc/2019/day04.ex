ab1287a1115e7ae170efa77b2eb2d1bb991bc913
qhwa <qhwa@163.com>
2019-12-04 14:02:44 +0800

    refactor

--- lib/aoc/2019/day04.ex
+++ lib/aoc/2019/day04.ex
@@ -4,46 +4,37 @@ defmodule AOC.Y2019.Day04 do
   """
 
   @input 147_981..691_423
+
   def part1 do
     @input
-    |> Enum.filter(&valid?/1)
-    |> length()
+    |> Stream.map(&Integer.digits/1)
+    |> Stream.filter(&valid_in_part1?/1)
+    |> Enum.count()
   end
 
   def part2 do
     @input
-    |> Stream.filter(&valid?/1)
+    |> Stream.map(&Integer.digits/1)
     |> Stream.filter(&valid_in_part2?/1)
     |> Enum.count()
   end
 
-  defp valid?(n) do
-    digits = Integer.digits(n)
+  defp valid_in_part1?(digits) do
     never_decrease?(digits) && repeated?(digits)
   end
 
-  defp valid_in_part2?(n) do
-    digits = Integer.digits(n)
-    valid_repeating_parts(digits) != []
+  defp valid_in_part2?(digits) do
+    never_decrease?(digits) && valid_repeating_parts(digits) != []
   end
 
   defp never_decrease?([]), do: true
-
-  defp never_decrease?([head | tail]) do
-    never_decrease?(tail, head)
-  end
+  defp never_decrease?([head | tail]), do: never_decrease?(tail, head)
 
   defp never_decrease?([], _), do: true
   defp never_decrease?([head | _], prev) when head < prev, do: false
+  defp never_decrease?([head | tail], _), do: never_decrease?(tail, head)
 
-  defp never_decrease?([head | tail], _) do
-    never_decrease?(tail, head)
-  end
-
-  defp repeated?([head | tail]) do
-    repeated?(tail, head)
-  end
-
+  defp repeated?([head | tail]), do: repeated?(tail, head)
   defp repeated?([], _), do: false
   defp repeated?([head | _], head), do: true
   defp repeated?([head | tail], _), do: repeated?(tail, head)
@@ -56,11 +47,11 @@ defmodule AOC.Y2019.Day04 do
     Enum.filter(acc, fn {_, n} -> n == 2 end)
   end
 
-  defp valid_repeating_parts([head | tail], [{prev, n} | acc_tail]) do
-    if head == prev do
-      valid_repeating_parts(tail, [{prev, n + 1} | acc_tail])
-    else
-      valid_repeating_parts(tail, [{head, 1}, {prev, n} | acc_tail])
-    end
+  defp valid_repeating_parts([head | tail], [{head, n} | acc_tail]) do
+    valid_repeating_parts(tail, [{head, n + 1} | acc_tail])
+  end
+
+  defp valid_repeating_parts([head | tail], acc) do
+    valid_repeating_parts(tail, [{head, 1} | acc])
   end
 end
