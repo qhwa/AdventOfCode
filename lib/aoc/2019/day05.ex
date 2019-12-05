29166e7542c19b1eb566c46d88c6b323b655730f
qhwa <qhwa@163.com>
2019-12-05 23:56:53 +0800

    finish 2019 day 05 part 2

--- lib/aoc/2019/day05.ex
+++ lib/aoc/2019/day05.ex
@@ -12,33 +12,22 @@ defmodule AOC.Y2019.Day05 do
            |> Stream.map(&String.to_integer/1)
            |> to_map()
 
-  @valid_ops [
-    1,
-    2,
-    101,
-    102,
-    104,
-    1001,
-    1002,
-    1101,
-    1102
-  ]
-
   def p1 do
     run_program(@program, 0, 1)
   end
 
-  def part2 do
+  def p2 do
+    run_program(@program, 0, 5)
   end
 
   def example do
-    run_program(to_map([3, 0, 4, 0, 99]), 0, 1)
+    run_program(to_map([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]), 0, 9)
   end
 
   def run_program(program, p, input, output \\ []) do
     case program[p] do
       99 ->
-        {:halt, output |> Enum.reverse()}
+        output |> Enum.reverse()
 
       3 ->
         run_program(
@@ -48,34 +37,80 @@ defmodule AOC.Y2019.Day05 do
           output
         )
 
-      4 ->
+      op when rem(op, 10) == 4 ->
         run_program(
           program,
           p + 2,
           input,
-          [program[program[p + 1]] | output]
+          [read(program, mode(op), p + 1) | output]
         )
 
-      104 ->
+      op when rem(op, 10) == 5 ->
         run_program(
           program,
-          p + 2,
+          goto(program, p, op, &(&1 != 0)),
           input,
-          [program[p + 1] | output]
+          output
         )
 
-      op when op in @valid_ops ->
+      op when rem(op, 10) == 6 ->
+        run_program(
+          program,
+          goto(program, p, op, &(&1 == 0)),
+          input,
+          output
+        )
+
+      op when rem(op, 10) == 7 ->
+        program
+        |> compare(p, op, &(&1 < &2))
+        |> run_program(p + 4, input, output)
+
+      op when rem(op, 10) == 8 ->
+        program
+        |> compare(p, op, &(&1 == &2))
+        |> run_program(p + 4, input, output)
+
+      op when rem(op, 10) in 1..2 ->
         op
         |> calc(program, p)
         |> run_program(p + 4, input, output)
     end
   end
 
+  defp mode(op, pos \\ 0) do
+    op
+    |> rem(floor(:math.pow(10, pos + 3)))
+    |> div(floor(:math.pow(10, pos + 2)))
+  end
+
+  defp read(program, 0, p), do: program[program[p]]
+  defp read(program, 1, p), do: program[p]
+
+  defp goto(program, p, op, if_jump) do
+    value = read(program, mode(op), p + 1)
+
+    if if_jump.(value) do
+      read(program, mode(op, 1), p + 2)
+    else
+      p + 3
+    end
+  end
+
+  defp compare(program, p, op, fun) do
+    r1 = read(program, mode(op), p + 1)
+    r2 = read(program, mode(op, 1), p + 2)
+    wp = program[p + 3]
+
+    ret = if fun.(r1, r2), do: 1, else: 0
+    %{program | wp => ret}
+  end
+
   defp calc(op, program, p) do
     {p1, p2, p3} = {p + 1, p + 2, p + 3}
 
-    r1 = if div(rem(op, 1000), 100) == 1, do: program[p1], else: program[program[p1]]
-    r2 = if div(rem(op, 10000), 1000) == 1, do: program[p2], else: program[program[p2]]
+    r1 = read(program, mode(op), p1)
+    r2 = read(program, mode(op, 1), p2)
     wp = program[p3]
 
     %{program | wp => execute(rem(op, 100), r1, r2)}
---
+++ test/aoc/2019/day05_test.exs
@@ -0,0 +1,89 @@
+defmodule Y2019.Day05Test do
+  import AOC.Y2019.Day05
+  import Helper.MyList
+
+  use ExUnit.Case
+
+  describe "run_program/1" do
+    test "it works" do
+      assert run([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 8) == [1]
+      assert run([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 2) == [0]
+
+      assert run([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 7) == [1]
+      assert run([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 9) == [0]
+
+      assert run([3, 3, 1108, -1, 8, 3, 4, 3, 99], 9) == [0]
+      assert run([3, 3, 1108, -1, 8, 3, 4, 3, 99], 8) == [1]
+
+      assert run([3, 3, 1107, -1, 8, 3, 4, 3, 99], 9) == [0]
+      assert run([3, 3, 1107, -1, 8, 3, 4, 3, 99], 7) == [1]
+    end
+
+    test "jumps work" do
+      assert run([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 3) == [1]
+      assert run([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 0) == [0]
+    end
+
+    test "integration" do
+      p = [
+        3,
+        21,
+        1008,
+        21,
+        8,
+        20,
+        1005,
+        20,
+        22,
+        107,
+        8,
+        21,
+        20,
+        1006,
+        20,
+        31,
+        1106,
+        0,
+        36,
+        98,
+        0,
+        0,
+        1002,
+        21,
+        125,
+        20,
+        4,
+        20,
+        1105,
+        1,
+        46,
+        104,
+        999,
+        1105,
+        1,
+        46,
+        1101,
+        1000,
+        1,
+        20,
+        4,
+        20,
+        1105,
+        1,
+        46,
+        98,
+        99
+      ]
+
+      assert run(p, 8) == [1000]
+      assert run(p, 1) == [999]
+      assert run(p, 9) == [1001]
+    end
+  end
+
+  defp run(program, input) do
+    program
+    |> to_map()
+    |> run_program(0, input)
+  end
+end
