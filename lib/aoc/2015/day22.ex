042294b6d7f3d1a4085fb8b989fbced949ea13f6
qhwa <qhwa@163.com>
2019-12-05 13:39:04 +0800

    temp save 2015 day 22

--- lib/aoc/2015/day22.ex
+++ lib/aoc/2015/day22.ex
@@ -3,8 +3,8 @@ defmodule AOC.Y2015.Day22 do
   @see https://adventofcode.com/2015/day/22
   """
 
-  @boss %{hp: 71, damage: 10}
   @hero %{hp: 50, armor: 0, mana: 500}
+  @boss %{hp: 71, damage: 10}
 
   @spells [
     magic_missile: 53,
@@ -14,181 +14,259 @@ defmodule AOC.Y2015.Day22 do
     recharge: 229
   ]
 
-  def puzzle do
-    @spells
-    |> Helper.MyList.perms()
-    |> Enum.map(fn perm ->
-      perm
-      |> emu_game(@hero, @boss, [], :hero)
+  def example do
+    # eample 1
+    # hero = %{hp: 10, armor: 0, mana: 250}
+    # boss = %{hp: 13, damage: 8}
+
+    # eample 2
+    hero = %{hp: 10, armor: 0, mana: 250}
+    boss = %{hp: 14, damage: 8}
+
+    [{0, hero, boss, [], []}]
+    |> Stream.iterate(&map_next_round/1)
+    |> Stream.take_while(&continue?/1)
+    |> Enum.to_list()
+    |> List.flatten()
+    |> Enum.find(&win?/1)
+    |> replay(hero, boss)
+  end
+
+  def p1 do
+    [{0, @hero, @boss, [], []}]
+    |> Stream.iterate(&map_next_round/1)
+    |> Stream.take_while(&continue?/1)
+    |> Enum.to_list()
+    |> List.flatten()
+    |> Enum.find(&win?/1)
+  end
+
+  defp map_next_round(solutions) do
+    solutions
+    |> Enum.flat_map(fn
+      {round, hero, boss, effects, used_spells} ->
+        for spell <- possible_spells(hero.mana, effects) do
+          case spell do
+            :none ->
+              {round, :lose, used_spells}
+
+            _ ->
+              play_round(round, spell, hero, boss, effects, used_spells)
+          end
+        end
+
+      {_round, :win, _used} ->
+        [:halt]
+
+      other ->
+        [other]
     end)
   end
 
-  def emu_game(_, %{hp: hp}, _, _, _) when hp <= 0 do
-    :lose
-  end
+  defp possible_spells(mana, effects) do
+    spells =
+      for {name, cost} <- @spells, cost <= mana, Keyword.get(effects, name, 0) == 0 do
+        {name, cost}
+      end
 
-  def emu_game(_, _, %{hp: hp}, _, _) when hp <= 0 do
-    :win
-  end
+    case {spells, effects} do
+      {[], []} ->
+        # IO.puts("player loses because out of mana (#{mana}), and non effects available")
+        [:none]
 
-  def emu_game([], _, _, _, :hero) do
-    :unknow
-  end
+      {[], _} ->
+        [wait: 0]
 
-  def emu_game([spell | tail], hero, boss, effects, :hero) do
-    # step 1: apply effects
-    {effects, {h, b}} = take_effects(effects, hero, boss)
-
-    # step 2: attack!
-    case cast_spell(spell, h, b, effects) do
-      {hero, boss, new_effects} ->
-        {_, cost} = spell
-
-        emu_game(
-          tail,
-          Map.update!(hero, :mana, &(&1 - cost)),
-          boss,
-          effects ++ new_effects,
-          :boss
-        )
-
-      ret when is_atom(ret) ->
-        ret
+      _ ->
+        spells
     end
   end
 
-  def emu_game(spells, hero, boss, effects, :boss) do
-    # step 1: apply effects
-    {effects, {h, b}} = take_effects(effects, hero, boss)
+  # win
+  defp play_round(round, _, _, %{hp: boss_hp}, _, solution) when boss_hp <= 0 do
+    {round, :win, solution}
+  end
 
-    # step 2: attack!
-    case boss_attack(b, h) do
-      :lose ->
-        :lose
+  # lose
+  defp play_round(round, _, %{hp: hp}, _, _, solution) when hp <= 0 do
+    {round, :lose, solution}
+  end
 
-      hero ->
-        emu_game(spells, hero, boss, effects, :hero)
+  # fight
+  defp play_round(round, {name, cost}, hero, boss, effects, solution) do
+    # IO.inspect({hero, boss, effects}, label: round)
+
+    IO.puts("-- Player turn --")
+    IO.puts("- Player has #{hero.hp} hit points, #{hero.armor} armor, #{hero.mana} mana")
+    IO.puts("- Boss has #{boss.hp} hit points")
+
+    with {1, {effects, {h, b}}} <- {1, take_effects(effects, hero, boss, solution)},
+         {:boss_alive, true} <- {:boss_alive, b.hp > 0},
+         # IO.inspect("boss still alive after effects", label: round),
+
+         {2, {h, b, new_effects}} <- {2, cast(name, h, b)},
+         {:boss_alive, true} <- {:boss_alive, b.hp > 0},
+         # IO.inspect("hero still alive after boss attack", label: round),
+
+         {3, {effects, {h, b}}} <- {3, take_effects(effects ++ new_effects, h, b, solution)},
+         {:boss_alive, true} <- {:boss_alive, b.hp > 0},
+         # IO.inspect("hero still alive after boss attack", label: round) do
+
+         {4, h} <- {4, boss_attack(b, h)},
+         {:hero_alive, true} <- {:hero_alive, h.hp > 0} do
+      # IO.inspect("hero still alive after boss attack", label: round) do
+
+      # IO.inspect("#{inspect(effects ++ new_effects)} after casting #{inspect name}", label: round)
+      {
+        round + 1,
+        h |> Map.update!(:mana, &(&1 - cost)),
+        b,
+        effects,
+        [name | solution]
+      }
+    else
+      {:boss_alive, false} ->
+        # IO.puts("win after #{inspect [name | solution]}")
+        {round, :win, [name | solution]}
+
+      {:hero_alive, false} ->
+        {round, :lose, [name | solution]}
     end
   end
 
-  def take_effects(effects, hero, boss) do
+  defp take_effects(effects, hero, boss, _solution) do
     effects
-    |> Enum.flat_map_reduce({hero, boss}, fn {effect, t}, {h, b} ->
-      apply_effect({effect, t - 1}, h, b)
+    |> Enum.flat_map_reduce({hero, boss}, fn effect, {h, b} ->
+      # IO.inspect(solution, label: inspect(effect))
+      {hero, boss} = apply_effect(effect, h, b)
+
+      case effect do
+        {_, 0} ->
+          {[], {hero, boss}}
+
+        {name, t} ->
+          {[{name, t - 1}], {hero, boss}}
+      end
     end)
   end
 
   def apply_effect({:shield, 0}, hero, boss) do
-    {
-      [],
-      {%{hero | armor: 0}, boss}
-    }
+    IO.puts("Shield wears off.")
+    {%{hero | armor: 0}, boss}
   end
 
-  def apply_effect({:shield, _} = effect, hero, boss) do
-    {
-      [effect],
-      {%{hero | armor: 7}, boss}
-    }
+  def apply_effect({:shield, t}, hero, boss) do
+    IO.puts("Shield's timer is now #{t}.")
+    {%{hero | armor: 7}, boss}
   end
 
-  def apply_effect({_, 0}, hero, boss) do
-    {
-      [],
-      {hero, boss}
-    }
+  def apply_effect({name, 0}, hero, boss) do
+    IO.puts("#{name} wears off.")
+    {hero, boss}
   end
 
-  def apply_effect({:drain, _} = effect, hero, boss) do
-    {
-      [effect],
-      {
-        Map.update!(hero, :hp, &(&1 + 2)),
-        Map.update!(boss, :hp, &(&1 - 2))
-      }
-    }
-  end
+  def apply_effect({:poison, t}, hero, boss) do
+    IO.puts("Poison deals 3 damage; its timer is now #{t}.")
 
-  def apply_effect({:poison, _} = effect, hero, boss) do
-    {
-      [effect],
-      {
-        hero,
-        Map.update!(boss, :hp, &(&1 - 3))
-      }
-    }
-  end
-
-  def apply_effect({:recharge, _} = effect, hero, boss) do
-    {
-      [effect],
-      {
-        Map.update!(hero, :mana, &(&1 + 101)),
-        boss
-      }
-    }
-  end
-
-  def cast_spell({name, _} = spell, hero, boss, effects) do
-    if Keyword.has_key?(effects, name) do
-      :effect_already_exists
-    else
-      cast_spell(spell, hero, boss)
-    end
-  end
-
-  def cast_spell(_, _, %{hp: hp}) when hp <= 0 do
-    :win
-  end
-
-  def cast_spell({_name, cost}, %{mana: mana}, _boss) when cost > mana do
-    :out_of_mana
-  end
-
-  def cast_spell({:magic_missile, _}, hero, boss) do
     {
       hero,
-      Map.update!(boss, :hp, &(&1 - 4)),
+      Map.update!(boss, :hp, &(&1 - 3))
+    }
+  end
+
+  def apply_effect({:recharge, t}, hero, boss) do
+    IO.puts("Recharge provides 101 mana; its timer is now #{t}.")
+
+    {
+      Map.update!(hero, :mana, &(&1 + 101)),
+      boss
+    }
+  end
+
+  def cast(:wait, hero, boss) do
+    {hero, boss, []}
+  end
+
+  def cast(:magic_missile, hero, boss) do
+    IO.puts("Player casts Magic Missile, dealing 4 damage.")
+
+    {
+      hero,
+      boss |> Map.update!(:hp, &(&1 - 4)),
       []
     }
   end
 
-  def cast_spell({:drain, _}, hero, boss) do
+  def cast(:drain, hero, boss) do
+    IO.puts("Player casts Drain, dealing 2 damage, and healing 2 hit points.")
+
     {
-      hero
-      |> Map.update!(:hp, &(&1 + 2)),
-      boss
-      |> Map.update!(:hp, &(&1 - 2)),
+      hero |> Map.update!(:hp, &(&1 + 2)),
+      boss |> Map.update!(:hp, &(&1 - 2)),
       []
     }
   end
 
-  def cast_spell({:shield, _}, hero, boss) do
+  def cast(:shield, hero, boss) do
     {
       hero,
-      boss
-      |> Map.update!(:hp, &(&1 - 2)),
+      boss |> Map.update!(:hp, &(&1 - 2)),
       [shield: 6]
     }
   end
 
-  def cast_spell({:poison, _}, hero, boss) do
+  def cast(:poison, hero, boss) do
     {hero, boss, [poison: 6]}
   end
 
-  def cast_spell({:recharge, _}, hero, boss) do
+  def cast(:recharge, hero, boss) do
+    IO.puts("Player casts Recharge.")
     {hero, boss, [recharge: 5]}
   end
 
-  def boss_attack(%{damage: damage}, %{hp: hp, armor: armor} = hero) do
-    dmg = max(armor - damage, 1)
+  defp boss_attack(%{damage: damage}, %{hp: hp, armor: armor} = hero) do
+    dmg = max(damage - armor, 1)
+    IO.puts("Boss attacks for #{damage} - #{armor} = #{dmg} damage!")
+    # IO.puts("boss attack: #{hp} -> #{hp - dmg} (shield: #{armor})")
+    %{hero | hp: hp - dmg}
+  end
 
-    case hp - dmg do
-      x when x <= 0 ->
-        :lose
+  defp continue?(solutions) do
+    should_halt = Enum.any?(solutions, &(&1 == :halt)) || Enum.all?(solutions, &lose?/1)
+    !should_halt
+  end
 
-      x ->
-        %{hero | hp: x}
-    end
+  defp win?({_, :win, _}) do
+    true
+  end
+
+  defp win?({_round, :lose, _}) do
+    false
+  end
+
+  defp win?({_round, _hero, %{hp: boss_hp}, _effects, _}) do
+    boss_hp <= 0
+  end
+
+  defp win?({_round, _hero, _boss, _effects, _}) do
+    false
+  end
+
+  defp lose?({_round, :lose, _}) do
+    true
+  end
+
+  defp lose?(_) do
+    false
+  end
+
+  defp replay({_, _, spells}, hero, boss) do
+    IO.puts("--------------- replay --------")
+
+    spells
+    |> Enum.reverse()
+    |> Enum.reduce({0, hero, boss, [], []}, fn spell, {round, hero, boss, effects, _} ->
+      play_round(round, {spell, Keyword.get(@spells, spell)}, hero, boss, effects, [])
+    end)
   end
 end
