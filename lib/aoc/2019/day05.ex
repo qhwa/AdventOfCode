6a70fca2b94ecf15e6d0bc8bc15ca01950d3f19d
qhwa <qhwa@163.com>
2019-12-06 01:37:07 +0800

    refactor

--- lib/aoc/2019/day05.ex
+++ lib/aoc/2019/day05.ex
@@ -12,6 +12,8 @@ defmodule AOC.Y2019.Day05 do
            |> Stream.map(&String.to_integer/1)
            |> to_map()
 
+  defguard is_read(op) when rem(op, 10) == 3
+
   def p1 do
     run(@program, %{pointer: 0, input: 1, output: []})
   end
@@ -35,72 +37,45 @@ defmodule AOC.Y2019.Day05 do
     end
   end
 
-  defp exec(data, 3, p, %{input: input}) do
-    {%{data[p + 1] => input}, p + 2, []}
+  # credo:disable-for-next-line Credo.Check.Refactor.CyclomaticComplexity
+  defp exec(data, op, p, %{input: input}) do
+    r1 = read(data, mode(op, 1), p + 1)
+    r2 = read(data, mode(op, 2), p + 2)
+    r3 = data[p + 3]
+
+    case rem(op, 10) do
+      1 ->
+        {%{r3 => r1 + r2}, p + 4, []}
+
+      2 ->
+        {%{r3 => r1 * r2}, p + 4, []}
+
+      3 ->
+        {%{data[p + 1] => input}, p + 2, []}
+
+      4 ->
+        {nil, p + 2, [r1]}
+
+      5 ->
+        {nil, (r1 != 0 && r2) || p + 3, []}
+
+      6 ->
+        {nil, (r1 == 0 && r2) || p + 3, []}
+
+      7 ->
+        {%{r3 => (r1 < r2 && 1) || 0}, p + 4, []}
+
+      8 ->
+        {%{r3 => (r1 == r2 && 1) || 0}, p + 4, []}
+    end
   end
 
-  defp exec(data, op, p, _) when rem(op, 10) == 4 do
-    {nil, p + 2, [read(data, mode(op), p + 1)]}
-  end
-
-  defp exec(data, op, p, _) when rem(op, 10) == 5 do
-    {nil, jump(data, p, op, &(&1 != 0)), []}
-  end
-
-  defp exec(data, op, p, _) when rem(op, 10) == 6 do
-    {nil, jump(data, p, op, &(&1 == 0)), []}
-  end
-
-  defp exec(data, op, p, _) when rem(op, 10) == 7 do
-    {compare(data, p, op, &((&1 < &2 && 1) || 0)), p + 4, []}
-  end
-
-  defp exec(data, op, p, _) when rem(op, 10) == 8 do
-    {compare(data, p, op, &((&1 == &2 && 1) || 0)), p + 4, []}
-  end
-
-  defp exec(data, op, p, _) when rem(op, 10) == 1 do
-    {calc(data, p, op, &(&1 + &2)), p + 4, []}
-  end
-
-  defp exec(data, op, p, _) when rem(op, 10) == 2 do
-    {calc(data, p, op, &(&1 * &2)), p + 4, []}
-  end
-
-  defp mode(op, pos \\ 0) do
+  defp mode(op, pos \\ 1) do
     op
-    |> rem(floor(:math.pow(10, pos + 3)))
-    |> div(floor(:math.pow(10, pos + 2)))
+    |> rem(floor(:math.pow(10, pos + 2)))
+    |> div(floor(:math.pow(10, pos + 1)))
   end
 
   defp read(data, 0, p), do: data[data[p]]
   defp read(data, 1, p), do: data[p]
-
-  defp jump(data, p, op, if_jump) do
-    value = read(data, mode(op), p + 1)
-
-    if if_jump.(value) do
-      read(data, mode(op, 1), p + 2)
-    else
-      p + 3
-    end
-  end
-
-  defp compare(data, p, op, fun) do
-    r1 = read(data, mode(op), p + 1)
-    r2 = read(data, mode(op, 1), p + 2)
-    wp = data[p + 3]
-
-    %{wp => fun.(r1, r2)}
-  end
-
-  defp calc(data, p, op, fun) do
-    {p1, p2, p3} = {p + 1, p + 2, p + 3}
-
-    r1 = read(data, mode(op), p1)
-    r2 = read(data, mode(op, 1), p2)
-    wp = data[p3]
-
-    %{wp => fun.(r1, r2)}
-  end
 end
