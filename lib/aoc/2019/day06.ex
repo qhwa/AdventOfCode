b356643192af7fc827efc306852dc4cd882800de
qhwa <qhwa@163.com>
2019-12-06 19:28:54 +0800

    finish day 06 part 2

--- lib/aoc/2019/day06.ex
+++ lib/aoc/2019/day06.ex
@@ -3,27 +3,12 @@ defmodule AOC.Y2019.Day06 do
   @see https://adventofcode.com/2019/day/6
   """
 
-  def p1, do: File.read!("priv/data/2019/day06.txt") |> parse() |> walk(:COM)
-  def p2, do: nil
+  def p1, do: read_input() |> walk(:COM)
+  def p2, do: read_input() |> min_distance(:YOU, :SAN)
 
-  def example do
-    input = """
-    COM)B
-    B)C
-    C)D
-    D)E
-    E)F
-    B)G
-    G)H
-    D)I
-    E)J
-    J)K
-    K)L
-    """
-
-    input
+  def read_input do
+    File.read!("priv/data/2019/day06.txt")
     |> parse()
-    |> walk(:COM)
   end
 
   def parse(str) do
@@ -34,10 +19,8 @@ defmodule AOC.Y2019.Day06 do
     |> Stream.map(fn arr -> Enum.map(arr, &String.to_atom/1) end)
     |> Enum.reduce(%{}, fn [c, a], map ->
       map
-      |> Map.update(c, %{children: [a]}, fn obj ->
-        %{obj | children: [a | obj.children]}
-      end)
-      |> Map.put_new(a, %{children: []})
+      |> Map.update(c, %{children: [a]}, &%{&1 | children: [a | &1.children]})
+      |> Map.update(a, %{children: [], parent: c}, &Map.put(&1, :parent, c))
     end)
   end
 
@@ -52,5 +35,33 @@ defmodule AOC.Y2019.Day06 do
       end)
 
     {length(node.children) + n1, n1 + n2}
+  end
+
+  def min_distance(map, a, b) do
+    path1 = ancestors(map, a)
+    path2 = ancestors(map, b)
+
+    for ancestor <- path1, ^ancestor <- path2 do
+      Enum.find_index(path1, &(&1 == ancestor)) +
+        Enum.find_index(path2, &(&1 == ancestor))
+    end
+    |> Enum.min()
+    |> Kernel.-(2)
+  end
+
+  def ancestors(map, start) do
+    ancestors(map, start, [])
+  end
+
+  def ancestors(_map, nil, path) do
+    path |> Enum.reverse()
+  end
+
+  def ancestors(map, current, path) do
+    ancestors(
+      map,
+      Map.get(map[current], :parent),
+      [current | path]
+    )
   end
 end
