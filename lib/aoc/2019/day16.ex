d892220cb59ca3c77ef7983857d232d38ef20ac4
qhwa <qhwa@163.com>
2019-12-17 00:22:17 +0800

    tmp save

--- lib/aoc/2019/day16.ex
+++ lib/aoc/2019/day16.ex
@@ -5,6 +5,28 @@ defmodule AOC.Y2019.Day16 do
 
   import Helper.MyList, only: [to_map: 1]
 
+  for len <- [8, 32, 650], pseed <- 1..len do
+    def pos_group(unquote(len), unquote(pseed)) do
+      unquote(
+        0..len
+        |> Enum.chunk_every(pseed)
+        |> Enum.drop(1)
+        |> Enum.take_every(4)
+        |> List.flatten()
+      )
+    end
+
+    def neg_group(unquote(len), unquote(pseed)) do
+      unquote(
+        0..len
+        |> Enum.chunk_every(pseed)
+        |> Enum.drop(3)
+        |> Enum.take_every(4)
+        |> List.flatten()
+      )
+    end
+  end
+
   def p1 do
     "priv/data/2019/day16.txt"
     |> File.read!()
@@ -12,47 +34,45 @@ defmodule AOC.Y2019.Day16 do
     |> String.to_charlist()
     |> Enum.map(&(&1 - ?0))
     |> apply_phase(100)
+    |> first_eight_digits()
   end
 
   def p2 do
+    "priv/data/2019/day16.txt"
+    |> File.read!()
+    |> String.trim()
+    |> String.to_charlist()
+    |> Enum.map(&(&1 - ?0))
+    |> read_message()
+  end
+
+  def read_message(digits) do
+    offset = digits |> Enum.take(7) |> Integer.undigits()
+    digits |> apply_phase(100) |> first_eight_digits(offset)
   end
 
   def apply_phase(digits, step) when is_list(digits) do
     apply_phase(to_map(digits), Enum.count(digits), step)
   end
 
-  def apply_phase(digits, _, 0), do: Enum.map(0..7, &digits[&1])
+  def apply_phase(digits, _, 0), do: digits
 
   def apply_phase(digits, len, step) do
     1..len
     |> Enum.map(fn pseed ->
-      digest(digits, len, pseed, 0)
+      last_digit(
+        Enum.sum(pos_group(len, pseed) |> Enum.map(&digits[&1 - 1])) -
+          Enum.sum(neg_group(len, pseed) |> Enum.map(&digits[&1 - 1]))
+      )
     end)
     |> apply_phase(step - 1)
   end
 
-  defp digest(_, 0, _, acc), do: last_digit(acc)
-
-  defp digest(digits_map, i, pseed, acc) do
-    case friction(pseed, i - 1) do
-      0 ->
-        digest(digits_map, i - 1, pseed, acc)
-
-      n ->
-        digest(digits_map, i - 1, pseed, acc + n * Map.fetch!(digits_map, i - 1))
-    end
-  end
-
-  defp friction(pseed, id) do
-    case rem(div(id + 1, pseed), 4) do
-      0 -> 0
-      1 -> 1
-      2 -> 0
-      3 -> -1
-    end
-  end
-
   defp last_digit(n) do
     rem(abs(n), 10)
+  end
+
+  def first_eight_digits(map, offset \\ 0) do
+    0..7 |> Enum.map(&map[&1 + offset])
   end
 end
--- test/aoc/2019/day16_test.exs
+++ test/aoc/2019/day16_test.exs
@@ -6,19 +6,29 @@ defmodule Y2019.Day16Test do
 
   describe "apply_phase/2" do
     test "it works" do
-      assert apply_phase([1, 2, 3, 4, 5, 6, 7, 8], 1) == digits(48_226_158)
-      assert apply_phase([1, 2, 3, 4, 5, 6, 7, 8], 2) == digits(34_040_438)
-      assert apply_phase([1, 2, 3, 4, 5, 6, 7, 8], 3) == [0 | digits(3_415_518)]
-      assert apply_phase([1, 2, 3, 4, 5, 6, 7, 8], 4) == [0 | digits(1_029_498)]
+      assert phase([1, 2, 3, 4, 5, 6, 7, 8], 1) == digits(48_226_158)
+      assert phase([1, 2, 3, 4, 5, 6, 7, 8], 2) == digits(34_040_438)
+      assert phase([1, 2, 3, 4, 5, 6, 7, 8], 3) == [0 | digits(3_415_518)]
+      assert phase([1, 2, 3, 4, 5, 6, 7, 8], 4) == [0 | digits(1_029_498)]
 
-      assert apply_phase(digits(80_871_224_585_914_546_619_083_218_645_595), 100) ==
+      assert phase(digits(80_871_224_585_914_546_619_083_218_645_595), 100) ==
                digits(24_176_176)
 
-      assert apply_phase(digits(19_617_804_207_202_209_144_916_044_189_917), 100) ==
+      assert phase(digits(19_617_804_207_202_209_144_916_044_189_917), 100) ==
                digits(73_745_418)
 
-      assert apply_phase(digits(69_317_163_492_948_606_335_995_924_319_873), 100) ==
+      assert phase(digits(69_317_163_492_948_606_335_995_924_319_873), 100) ==
                digits(52_432_133)
     end
+  end
+
+  describe "read_message/1" do
+    test "it works" do
+      # assert read_message([0 | digits(03036732577212944063491565474664)]) == digits(84462026)
+    end
+  end
+
+  defp phase(digits, count) do
+    apply_phase(digits, count) |> first_eight_digits()
   end
 end
