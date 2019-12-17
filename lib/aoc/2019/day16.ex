2c7d6388c4a06c63f43c781a6eff736c82d49ae3
qhwa <qhwa@163.com>
2019-12-17 08:47:58 +0800

    improve

--- lib/aoc/2019/day16.ex
+++ lib/aoc/2019/day16.ex
@@ -3,7 +3,7 @@ defmodule AOC.Y2019.Day16 do
   @see https://adventofcode.com/2019/day/16
   """
 
-  for size <- [8, 32, 650], pseed <- 1..size do
+  for size <- [8, 650, 6_500_000], pseed <- 1..size do
     def pos_group(unquote(size), unquote(pseed)) do
       unquote(
         0..size
@@ -25,6 +25,22 @@ defmodule AOC.Y2019.Day16 do
     end
   end
 
+  def pos_group(size, pseed) do
+    0..size
+    |> Enum.chunk_every(pseed)
+    |> Enum.drop(1)
+    |> Enum.take_every(4)
+    |> List.flatten()
+  end
+
+  def neg_group(size, pseed) do
+    0..size
+    |> Enum.chunk_every(pseed)
+    |> Enum.drop(3)
+    |> Enum.take_every(4)
+    |> List.flatten()
+  end
+
   def p1 do
     "priv/data/2019/day16.txt"
     |> File.read!()
@@ -41,6 +57,7 @@ defmodule AOC.Y2019.Day16 do
     |> String.trim()
     |> String.to_charlist()
     |> Enum.map(&(&1 - ?0))
+    |> List.duplicate(10_000)
     |> read_message()
   end
 
@@ -51,33 +68,32 @@ defmodule AOC.Y2019.Day16 do
 
   def apply_phase(digits, step) when is_list(digits) do
     digits = List.to_tuple(digits)
-    size = tuple_size(digits)
-    apply_phase(digits, size, step)
+    apply_phase(digits, step)
   end
 
-  def apply_phase(digits, _, 0), do: digits
+  def apply_phase(digits, 0), do: digits
 
-  def apply_phase(digits, size, step) do
+  def apply_phase(digits, step) do
+    size = tuple_size(digits)
+
     1..size
     |> Enum.reduce(digits, fn pseed, acc ->
       pos =
         pos_group(size, pseed)
-        |> Enum.map(&elem(digits, &1 - 1))
-        |> Enum.sum()
+        |> Enum.reduce(0, &(elem(digits, &1 - 1) + &2))
 
       neg =
         neg_group(size, pseed)
-        |> Enum.map(&elem(digits, &1 - 1))
-        |> Enum.sum()
+        |> Enum.reduce(0, &(elem(digits, &1 - 1) + &2))
 
       put_elem(acc, pseed - 1, last_digit(pos - neg))
     end)
-    |> apply_phase(size, step - 1)
+    |> apply_phase(step - 1)
   end
 
   defp last_digit(n), do: rem(abs(n), 10)
 
-  def first_eight_digits(result, _offset \\ 0) do
-    result |> Tuple.to_list() |> Enum.take(8)
+  def first_eight_digits(result, offset \\ 0) do
+    0..7 |> Enum.map(&elem(result, offset + &1))
   end
 end
